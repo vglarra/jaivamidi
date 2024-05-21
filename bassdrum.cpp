6e854afa7df8cdbb15c7db0d50763327aeb7185b
Victor Larraguibel <vlarraguibel@hotmail.com>
2024-05-20 22:04:05 -0400

    first commit on new file bassdrum with working code

---
+++ bassdrum.cpp
@@ -0,0 +1,36 @@
+#include <Control_Surface.h>
+USBMIDI_Interface midi;
+
+// Push button connected between pin 2 and ground.
+// Note message is sent when pressed.
+Button pushbutton {2};
+
+// MIDI address of the note to send
+const MIDIAddress noteAddress {MIDI_Notes::C[4], Channel_1};
+
+void setup() {
+  Control_Surface.begin();
+  pushbutton.begin(); // enables internal pull-up
+  midi.begin();       // initialize the MIDI interface
+  Serial.begin(9600);
+}
+
+void loop() {
+  Control_Surface.loop();
+  midi.update(); // read and handle or discard MIDI input
+  pushbutton.update(); // read the button state
+
+  // Read the analog signal from A0
+  int analogValue = analogRead(A0);
+
+  // Map the analog value to a MIDI velocity (0-127)
+  uint8_t velocity = map(analogValue, 0, 1023, 0, 127);
+
+  if (pushbutton.getState() == Button::Falling) {
+    midi.sendNoteOn(noteAddress, velocity);
+    Serial.println(String(velocity));
+  } // send a note on event
+  else if (pushbutton.getState() == Button::Rising) {
+    midi.sendNoteOff(noteAddress, velocity);
+  } // send a note off event
+}
